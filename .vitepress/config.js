e0bd100aaa11d6017a149db31e54d73dab4021c0
taylorc <taylorc5150@gmail.com>
2021-12-01 18:26:05 +1100

    initial blog set up in vitepress

---
+++ .vitepress/config.js
@@ -0,0 +1,39 @@
+const { getPosts, generatePaginationPages } = require('./theme/serverUtils')
+
+async function config() {
+    const pageSize = 2
+    await generatePaginationPages(pageSize)
+    return {
+        title: 'vitepress-blog',
+        base:'/',
+        description: 'vitepress,blog,blog-theme',
+        themeConfig: {
+            posts: await getPosts(),
+            pageSize: pageSize,
+            website: 'https://github.com/airene/vitepress-blog-pure', //copyright link
+            comment: {
+                repo: 'airene/vitepress-blog-pure',
+                themes: 'github-light',
+                issueTerm: 'pathname'
+            },
+            nav: [
+                { text: 'Home', link: '/' },
+                { text: 'Archives', link: '/pages/archives' },
+                { text: 'Tags', link: '/pages/tags' },
+                { text: 'About', link: '/pages/about' }
+                // { text: 'Airene', link: 'http://airene.net' }  -- External link test
+            ]
+        },
+        srcExclude: ['README.md'] // exclude the README.md , needn't to compiler
+        /*
+        vite: {
+            build: { minify: false }
+        },
+        optimizeDeps: {
+            keepNames: true
+        }
+        */
+    }
+}
+
+module.exports = config()
---
+++ posts/vitepress-first.md
@@ -0,0 +1,14 @@
+---
+date: 2021-06-30
+title: About Me
+tags:
+- bio
+- Craig Taylor
+description: More about me
+---
+Me Me Me
+
+
+<!-- <Comment /> -->
+
+
